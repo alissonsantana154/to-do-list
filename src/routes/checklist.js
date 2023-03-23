8a99401f9eb6fcf3785691abc3b49b2aba3b5b32
Alisson Santana <alissonsantana16608@gmail.com>
2023-03-23 11:04:03 -0300

    Rotas PUT e DELETE

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -12,8 +12,18 @@ router.post("/", (req, res) => {
 });
 
 router.get("/:id", (req, res) => {
-  console.log(req.params);
-  res.send(`ID: ${req.params.title}`);
+  console.log(req.body);
+  res.send(`ID: ${req.params.id}`);
+});
+
+router.put("/:id", (req, res) => {
+  console.log(req.bory);
+  res.send(`PUT ID: ${req.params.id}`);
+});
+
+router.delete("/:id", (req, res) => {
+  console.log(req.bory);
+  res.send(`DELETE ID: ${req.params.id}`);
 });
 
 module.exports = router;
